bb4409f330499ba074f2fee6a4d4d2557d53ced1
Chris <cbzoozoom7@gmail.com>
2023-03-19 21:39:17 -0500

    wrote the HashTable class prototype & a stub for each method.

--- hashtable.cpp
+++ hashtable.cpp
@@ -2,3 +2,27 @@
 //Chris Bowman
 //Created 19 Mar 2023
 #include "hashtable.h"
+HashTable::HashTable() {
+
+}
+HashTable::~HashTable() {
+    
+}
+bool HashTable::insertEntry(int id, string *data) {
+    return false;
+}
+string HashTable::getData(int id) {
+    return "";
+}
+bool HashTable::removeEntry(int id) {
+    return false;
+}
+int HashTable::getCount() {
+    return -1;
+}
+void HashTable::printTable() {
+
+}
+int HashTable::hash(int id) {
+    return -1;
+}
--- hashtable.h
+++ hashtable.h
@@ -3,4 +3,18 @@
 //Created 19 Mar 2023
 #ifndef HASHTABLE_H
     #define HASHTABLE_H
+    #include <string>
+    using std::string;
+    class HashTable {
+        public:
+            HashTable();
+            ~HashTable();
+            bool insertEntry(int, string*);
+            string getData(int);
+            bool removeEntry(int);
+            int getCount();
+            void printTable();
+        private:
+            int hash(int);
+    };
 #endif
